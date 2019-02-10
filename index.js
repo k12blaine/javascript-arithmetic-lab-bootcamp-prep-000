99b3d0a6b1574c57ee502fb3c4bd6b752c8db12a
Kiana Blaine <kiana.blaine1@gmail.com>
2019-02-10 01:41:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-fun
+function add(a,b)
+{
+  return a+b
+}
+function subtract(a,b)
+{
+  return a-b
+}
+function multiply(a,b)
+{
+  return a*b
+}
+function divide(a,b)
+{
+  return a/b
+}
+function inc(n)
+{
+  n++
+  return n
+}
